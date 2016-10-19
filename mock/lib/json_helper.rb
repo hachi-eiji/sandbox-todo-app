5824a39a1726de7e7d0ebc3a677df4277e176605
Eiji Hachiya <hachiyae@gmail.com>
2016-10-20 02:01:21 +0900

    middlemanを入れる

--- mock/lib/json_helper.rb
+++ mock/lib/json_helper.rb
@@ -7,17 +7,17 @@ class JsonHelper
 
   # /api以下にアクセスするとレスポンスヘッダを書き換え
   def call(env)
-    if (match = %r(\A/api/(.*)\Z).match(env['REQUEST_PATH']))
-      path = "./data/#{match[1]}.json"
-      unless File.exist?(path)
-        puts "#{path} not found"
-        return [404, { 'Content-Type' => 'application/json' }, [{}.to_json]]
-      end
-      File.open(path) do |file|
-        [200, { 'Content-Type' => 'application/json' }, [JSON.load(file).to_json]]
-      end
-    else
-      @app.call(env)
+    unless (match = %r(\A/api/(.*)\Z).match(env['REQUEST_PATH']))
+      return @app.call(env)
+    end
+
+    path = "./data/#{match[1]}.json"
+    unless File.exist?(path)
+      puts "#{path} not found"
+      return [404, { 'Content-Type' => 'application/json' }, [{}.to_json]]
+    end
+    File.open(path) do |file|
+      [200, { 'Content-Type' => 'application/json' }, [JSON.load(file).to_json]]
     end
   end
 end

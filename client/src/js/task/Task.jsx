83c8bda5e1d2f44e3b110709a516c4597c47ff85
Eiji Hachiya <hachiyae@gmail.com>
2016-10-30 13:58:01 +0900

    完了処理

--- client/src/js/task/Task.jsx
+++ client/src/js/task/Task.jsx
@@ -4,26 +4,41 @@ import {binds, postJSON} from '../common/util';
 class Task extends React.Component {
   constructor(props) {
     super(props);
-    binds(this, 'handleDeleteTask');
+    binds(this, 'handleDeleteTask', 'handleDone');
   }
 
   handleDeleteTask(e) {
     e.preventDefault();
     postJSON('/tasks/' + this.props.id, {}, {method: 'DELETE'})
       .then(res => {
-        this.props.handelDeleteTask(null, this.props.index);
+        this.props.handleDeleteRow(null, this.props.index, {status: 'success', message: '削除しました'});
       })
       .catch(e => {
-        this.props.handelDeleteTask(e);
+        this.props.handleDeleteRow(e);
       });
+  }
+
+  // doneイベント
+  handleDone(e) {
+    e.preventDefault();
+    postJSON('/tasks/' + this.props.id, {done: true}, {method: 'PUT'})
+      .then(res => {
+        this.props.handleDeleteRow(null, this.props.index, {status: 'success', message: '完了にしました'});
+      })
+      .catch(e => {
+        this.props.handleDeleteRow(e);
+      })
   }
 
   render() {
     return (
       <tr>
-        <td>{this.props.id}</td>
+        <td>
+          <label className="radio-inline">
+            <input type="radio" name="done" onChange={this.handleDone}/>
+          </label>
+        </td>
         <td>{this.props.title}</td>
-        <td>{this.props.description}</td>
         <td>{this.props.due_date}</td>
         <th>
           <button className="btn btn-danger" onClick={this.handleDeleteTask}>削除</button>
@@ -38,6 +53,6 @@ Task.propTypes = {
   title: React.PropTypes.string.isRequired,
   description: React.PropTypes.string,
   due_date: React.PropTypes.string,
-  handelDeleteTask: React.PropTypes.func
+  handleDeleteRow: React.PropTypes.func
 };
 export default Task
--- client/src/js/task/TaskList.jsx
+++ client/src/js/task/TaskList.jsx
@@ -9,10 +9,8 @@ class TaskList extends React.Component {
     binds(this, 'handleDeleteRow');
     this.state = {
       tasks: [],
-      delete: {
-        status: '',
-        message: ''
-      }
+      status: '',
+      message: ''
     }
   }
 
@@ -26,10 +24,12 @@ class TaskList extends React.Component {
       })
   }
 
-  handleDeleteRow(error, idx) {
+  handleDeleteRow(error, idx, notification) {
     if (error) {
-      this.setState({delete: {status: 'warning', message: '削除に失敗しました'}});
+      this.setState({status: 'warning', message: 'エラーが発生しました'});
     } else {
+      let _notification = {status: '', message: ''}
+      Object.assign(_notification, notification);
       this.setState((prevState, props) => {
         let newTasks = [];
         for (let i = 0; i < prevState.tasks.length; i++) {
@@ -38,28 +38,19 @@ class TaskList extends React.Component {
           }
           newTasks.push(prevState.tasks[i]);
         }
-        return {tasks: newTasks, delete: {status: '', message: ''}};
+        return {tasks: newTasks, status: _notification.status, message: _notification.message};
       });
     }
   }
 
   render() {
     const tasks = this.state.tasks.map((task, index) => {
-      return <Task key={task.id} {...task} index={index} handelDeleteTask={this.handleDeleteRow}/>
+      return <Task key={task.id} {...task} index={index} handleDeleteRow={this.handleDeleteRow}/>
     });
     return (
       <div>
-        <Alert type={this.state.delete.status} message={this.state.delete.message}/>
+        <Alert type={this.state.status} message={this.state.message}/>
         <table className="table table-hover">
-          <thead>
-          <tr>
-            <th>id</th>
-            <th>タイトル</th>
-            <th>説明</th>
-            <th>期日</th>
-            <th></th>
-          </tr>
-          </thead>
           <tbody>
           {tasks}
           </tbody>

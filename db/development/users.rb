aee58e15056a4eef73ef013c60ac0f91e1e12b3e
Eiji Hachiya <hachiyae@gmail.com>
2016-11-03 21:44:24 +0900

    password digestだった

--- db/development/users.rb
+++ db/development/users.rb
@@ -1,11 +1,10 @@
 require 'faker'
 
-User.delete_all
 10.times do
   User.create!(
     name:                  Faker::Name.last_name,
     email:                 Faker::Internet.safe_email,
-    password_digest:       'test',
+    password:              'test',
     password_confirmation: 'test',
     active:                true
   )

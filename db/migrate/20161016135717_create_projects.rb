3bca7ffd7ec01fba39d032af7e87461a159ced78
Eiji Hachiya <hachiyae@gmail.com>
2016-11-06 01:17:47 +0900

    使ってないので削除

--- db/migrate/20161016135717_create_projects.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjects < ActiveRecord::Migration
-  def change
-    create_table :projects do |t|
-      t.references :user, index: true, foreign_key: true, null: false
-      t.string :name, null: false, comment: 'プロジェクト名'
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20161016144351_create_task_assigns.rb
+++
@@ -1,9 +0,0 @@
-class CreateTaskAssigns < ActiveRecord::Migration
-  def change
-    create_table :task_assigns do |t|
-      t.references :task, foreign_key: true, null: false
-      t.references :user, foreign_key: true, null: false
-      t.timestamps null: false
-    end
-  end
-end

642c7edfa486845a43bf84d575eebc312e4620e1
Eiji Hachiya <hachiyae@gmail.com>
2016-11-06 23:57:47 +0900

    この辺は適当に入れておく

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :project
   has_many :task_assigns, dependent: :destroy
   has_many :task_notes, dependent: :destroy
+
+  validates :project_id, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :updater, presence: true
 end

c423d7885cbb7c52c8dd76e8971a798369dc4227
Eiji Hachiya <hachiyae@gmail.com>
2016-11-06 23:17:10 +0900

    軽く作ってみる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ActiveRecord::Base
   has_many :task_assigns, dependent: :destroy
   has_many :tasks, through: :task_assigns
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def before_create
+    self.active = false
+  end
+
+  def activate
+    update(active: true) unless active?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'name' do
+      it 'check presence' do
+        u = User.new(
+          email:                 'FOO@example.com',
+          password:              'test',
+          password_confirmation: 'test'
+        )
+        expect(u.valid?).to be false
+        expect(u.errors.messages[:name]).to eq(["can't be blank"])
+      end
+    end
+    context 'email' do
+      before do
+        User.create!(
+          name:                  'test',
+          email:                 'FOO@example.com',
+          password:              'test',
+          password_confirmation: 'test'
+        )
+      end
+      it 'check presence' do
+        u = User.new(
+          name:                  'test',
+          password:              'test',
+          password_confirmation: 'test'
+        )
+        expect(u.valid?).to be false
+        expect(u.errors.messages[:email]).to eq(["can't be blank"])
+      end
+      it '大文字小文字区別せずユニーク制約が有効になっている' do
+        user = User.new(
+          name:                  'test',
+          email:                 'FOO@example.com',
+          password:              'test',
+          password_confirmation: 'test'
+        )
+        expect(user.valid?).to be false
+      end
+      it 'emailのユニーク制約が有効' do
+        user = User.new(
+          name:                  'test',
+          email:                 'foo@example.com',
+          password:              'test',
+          password_confirmation: 'test'
+        )
+        expect(user.valid?).to be false
+      end
+    end
+  end
 end

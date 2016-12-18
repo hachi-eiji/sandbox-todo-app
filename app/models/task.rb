ae8f7d532f877180a5e2e316130996babd4cb38e
Eiji Hachiya <hachiyae@gmail.com>
2016-12-18 18:40:08 +0900

    リファクタリング

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,28 @@ class Task < ActiveRecord::Base
   validates :creator_id, presence: true
   validates :updater_id, presence: true
   validates :due_date, feature_date: true
+
+  def done
+    ActiveRecord::Base.transaction do
+      lock!
+      DoneTask.create!(
+        id:          id,
+        project_id:  project_id,
+        title:       title,
+        description: description,
+        due_date:    due_date,
+        creator_id:  creator_id,
+        updater_id:  updater_id
+      )
+      task_notes.each do |task_note|
+        DoneTaskNote.create!(id: task_note.id, task_id: task_note.task_id, user_id: task_note.user_id, note: task_note)
+      end
+
+      task_assigns.each do |task_assign|
+        DoneTaskAssign.create!(id: task_assign.id, task_id: task_assign.task_id, user_id: task_assign.user_id)
+      end
+      # まとめて削除する
+      destroy!
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,4 +10,63 @@ RSpec.describe Task, type: :model do
       end
     end
   end
+
+  describe '#done' do
+    context 'タスクのメモ/アサインがない' do
+      it 'タスクが存在しない' do
+        expect {
+          Task.find(0).done
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it 'タスクが存在する' do
+        create(:task)
+        task = Task.first
+        task.done
+        expect(Task.find_by(task.id)).to be_nil
+        expect(DoneTask.find_by(task.id)).not_to be_nil
+      end
+    end
+
+    context 'タスクのメモ/アサインが存在する' do
+      it 'タスクが存在する' do
+        task              = build(:task)
+        task.task_notes   = [build(:task_note), build(:task_note)]
+        task.task_assigns = [build(:task_assign), build(:task_assign)]
+        task.save!
+
+        id      = task.id
+        task.done
+
+        expect(Task.where(id: id).empty?).to be_truthy
+        expect(TaskNote.where(task_id: id).empty?).to be_truthy
+        expect(TaskAssign.where(task_id: id).empty?).to be_truthy
+        expect(DoneTask.where(id: id)).not_to be_nil
+        expect(DoneTaskNote.where(task_id: id)).not_to be_nil
+        expect(DoneTaskAssign.where(task_id: id)).not_to be_nil
+      end
+
+      it 'DoneTaskAssign実行時に例外が発生する' do
+        expect(DoneTaskAssign).to \
+          receive(:create!).with(any_args).and_raise('error')
+
+        task              = build(:task)
+        task.task_notes   = [build(:task_note), build(:task_note)]
+        task.task_assigns = [build(:task_assign), build(:task_assign)]
+        task.save!
+
+        id      = task.id
+        expect {
+          task.done
+        }.to raise_error('error')
+
+        expect(Task.where(id: id).empty?).to be_falsey
+        expect(TaskNote.where(task_id: id).empty?).to be_falsey
+        expect(TaskAssign.where(task_id: id).empty?).to be_falsey
+        expect(DoneTask.where(id: id).empty?).to be_truthy
+        expect(DoneTaskNote.where(task_id: id).empty?).to be_truthy
+        expect(DoneTaskAssign.where(task_id: id).empty?).to be_truthy
+      end
+    end
+  end
 end

5cf6744f482a6f8942d895538810919099f9ad47
Eiji Hachiya <hachiyae@gmail.com>
2017-01-03 14:47:25 +0900

    remove due_date validation
    Signed-off-by: Eiji Hachiya <hachiyae@gmail.com>

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe 'due_date' do
-    context 'validation' do
-      it 'error occurred when set past due_date' do
-        task = build(:task, due_date: Time.now - 1.days)
-        expect(task.valid?).to be false
-        expect(task.errors.messages[:due_date]).to include("can't include past date")
-      end
-    end
-  end
-
   describe '#done' do
     context 'タスクのメモ/アサインがない' do
       it 'タスクが存在しない' do

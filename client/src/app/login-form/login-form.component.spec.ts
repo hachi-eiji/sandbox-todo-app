321983b1bd4bb499e0005eaf4cf48361529fcf7a
Eiji Hachiya <hachiyae@gmail.com>
2017-07-16 10:45:51 +0900

    途中まで

--- client/src/app/login-form/login-form.component.spec.ts
+++ client/src/app/login-form/login-form.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { LoginFormComponent } from './login-form.component';
+import { AlertComponent } from '../alert/alert.component';
+
+import { TokenService } from '../common/token.service';
+import { LoginService } from './login.service';
+import { Login } from './login';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
@@ -8,7 +14,12 @@ describe('LoginFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
+      imports: [FormsModule],
+      providers: [
+        TokenService,
+        LoginService
+      ],
+      declarations: [LoginFormComponent, AlertComponent]
     })
     .compileComponents();
   }));
@@ -16,10 +27,15 @@ describe('LoginFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    component.model = new Login('', '');
+    component.alert = null;
+    const tokenService = fixture.debugElement.injector.get(TokenService);
+    spyOn(tokenService, 'get');
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });

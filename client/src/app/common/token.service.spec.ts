ff4f99a4b5fb6d4b1a221d85d46b9c1c292d028b
Eiji Hachiya <hachiyae@gmail.com>
2017-07-16 18:26:09 +0900

    add token service spec

--- client/src/app/common/token.service.spec.ts
+++ client/src/app/common/token.service.spec.ts
@@ -1,15 +1,27 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
 import { TokenService } from './token.service';
+import { HttpClient } from './HttpClient';
+import { TokenStorage } from './TokenStorage';
+import { ConnectionBackend, Http, HttpModule } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 describe('TokenService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TokenService]
+      imports: [HttpModule],
+      providers: [
+        TokenService,
+        HttpClient,
+        Http,
+        ConnectionBackend,
+        TokenStorage,
+      ],
     });
   });
 
-  it('should ...', inject([TokenService], (service: TokenService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should ...', async(inject([TokenService, HttpClient], (service: TokenService, httpClient: HttpClient) => {
+    spyOn(httpClient, 'getJson').and.returnValue(Observable.create(observable => observable.next()));
+    expect(service.get()).toBeUndefined();
+  })));
 });

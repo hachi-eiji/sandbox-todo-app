583890beaa41e8b272c82f38ed8d3df7af1255df
Eiji Hachiya <hachiyae@gmail.com>
2017-07-17 17:59:54 +0900

    add it

--- client/src/app/task/task.service.spec.ts
+++ client/src/app/task/task.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 import { TaskService } from './task.service';
 import { HttpClient } from '../common/HttpClient';
 import { TokenStorage } from '../common/TokenStorage';
+import { Task } from './Task';
 
 describe('TaskService', () => {
   beforeEach(() => {
@@ -15,5 +18,25 @@ describe('TaskService', () => {
 
   it('should ...', inject([TaskService], (service: TaskService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should get task list', inject([TaskService], (service: TaskService) => {
+    const tasks: Task[] = [];
+    for (let i = 0; i < 5; i++) {
+      tasks.push(
+        Task.create({
+          id: i,
+          title: `title_${i}`,
+          description: `description_${i}`,
+          due_date: new Date(2017, 2, 1)
+        })
+      );
+    }
+    spyOn(service, 'list').and.returnValue(Observable.of(tasks));
+
+    const result = service.list();
+    result.subscribe(subscribe => {
+      expect(subscribe.length).toEqual(5);
+    });
   }));
 });

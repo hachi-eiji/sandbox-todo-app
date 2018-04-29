279759da1ff505d0afb3584ff957c71b162f3bf0
Eiji Hachiya <hachiyae@gmail.com>
2018-04-29 14:41:39 +0900

    update CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:4200', '127.0.0.1:4200'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end

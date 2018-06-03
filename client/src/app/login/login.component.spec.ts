5202cff6383602df8a81a81b338896cc587395a5
hachi-eiji <hachiyae@gmail.com>
2018-06-04 00:50:24 +0900

    fixup add login component test

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
 
 import {LoginComponent} from './login.component';
+import {CoreModule} from '../core/core.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [FormsModule, CoreModule]
     })
     .compileComponents();
   }));
@@ -19,7 +22,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create component', () => {
+    expect(component.message).toEqual(undefined);
   });
 });

67c7768f09324f4ba2a27b729f7a703890b75206
hachi-eiji <hachiyae@gmail.com>
2018-06-03 21:17:11 +0900

    implement HttpService

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpService} from './http.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    HttpService
+  ]
 })
 export class CoreModule { }
---
+++ client/src/app/core/http.service.ts
@@ -0,0 +1,51 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs/internal/Observable';
+import {environment} from '../../environments/environment';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs/internal/observable/throwError';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  private static getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Accept': 'application/json'
+    });
+  }
+
+  private static createParams(data?: {}): HttpParams {
+    let params = new HttpParams();
+    if (!data) {
+      return params;
+    }
+    Object.keys(data).forEach(k => {
+      params = params.set(k, data[k]);
+    });
+    return params;
+  }
+
+  get<T>(path: string, data?: {}): Observable<T> {
+    const options = {
+      headers: HttpService.getHeaders(),
+      params: HttpService.createParams(data),
+      withCredentials: true
+    };
+    return this.httpClient.get<T>(environment.api.url + path, options)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      return throwError(error.error);
+    } else {
+      return throwError(error);
+    }
+  }
+}

e14fc5d3aca008db61682aba7b1e87bcf637bb8f
hachi-eiji <hachiyae@gmail.com>
2018-06-09 22:56:41 +0900

    generate tasks component

--- client/src/app/tasks/tasks.module.ts
+++ client/src/app/tasks/tasks.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TasksRoutingModule
   ],
-  declarations: []
+  declarations: [TasksComponent]
 })
 export class TasksModule { }

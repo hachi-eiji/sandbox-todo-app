017ba9574b382be2c89c6e53e1faee6364cb9d78
hachi-eiji <hachiyae@gmail.com>
2018-06-09 20:55:00 +0900

    fix login.service.spec.ts

--- client/src/app/login/shared/login.service.spec.ts
+++ client/src/app/login/shared/login.service.spec.ts
@@ -1,11 +1,13 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {LoginService} from './login.service';
+import {CoreModule} from '../../core/core.module';
 
 describe('LoginService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoginService]
+      providers: [LoginService],
+      imports: [CoreModule]
     });
   });
 

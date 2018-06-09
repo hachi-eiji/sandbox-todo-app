0b6731ab94dd5e166db31c18f6716d62d305e49d
hachi-eiji <hachiyae@gmail.com>
2018-06-09 23:01:38 +0900

    remove unused

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,19 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule, Routes} from '@angular/router';
-import {AlertComponent} from './alert/alert.component';
-import {AppComponent} from './app.component';
-import {HttpClientService} from './common/http-client.service';
-import {TokenStorageService} from './common/token-storage.service';
-import {TokenService} from './common/token.service';
-import {ConfirmModalComponent} from './confirm-modal/confirm-modal.component';
-import {CoreModule} from './core/core.module';
-import {LoginModule} from './login/login.module';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {TaskListComponent} from './task-list/task-list.component';
-import {TaskComponent} from './task/task.component';
-import {TaskService} from './task/task.service';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AlertComponent } from './alert/alert.component';
+import { AppComponent } from './app.component';
+import { TokenStorageService } from './common/token-storage.service';
+import { TokenService } from './common/token.service';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
+import { CoreModule } from './core/core.module';
+import { LoginModule } from './login/login.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
-  {path: 'tasks', component: TaskListComponent},
-  {path: '', redirectTo: '/tasks', pathMatch: 'full'},
-  {path: '**', component: NotFoundComponent}
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -26,9 +21,7 @@ const appRoutes: Routes = [
     AppComponent,
     AlertComponent,
     NotFoundComponent,
-    TaskListComponent,
     ConfirmModalComponent,
-    TaskComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,8 +33,6 @@ const appRoutes: Routes = [
   providers: [
     TokenService,
     TokenStorageService,
-    TaskService,
-    HttpClientService,
   ],
   bootstrap: [AppComponent]
 })

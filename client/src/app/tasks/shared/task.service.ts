c69d1a2e07615cb6ecd704d80d8d5bc8fc83dc6e
hachi-eiji <hachiyae@gmail.com>
2018-06-10 20:32:12 +0900

    implement task component

--- client/src/app/tasks/shared/task.service.ts
+++ client/src/app/tasks/shared/task.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpService } from '../../core/http/http.service';
+import { Tasks } from './tasks.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {
+  }
 
-  getList(): Observable<any> {
-    return this.httpService.get('/tasks');
+  getList(): Observable<Tasks> {
+    return this.httpService.get<Tasks>('/tasks');
   }
 }
--- client/src/app/tasks/tasks.component.ts
+++ client/src/app/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { TaskService } from './shared/task.service';
+import { Tasks } from './shared/tasks.model';
 
 @Component({
   selector: 'app-tasks',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  tasks$: Observable<Tasks>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private taskService: TaskService) {
   }
 
+  ngOnInit() {
+    this.fetch();
+  }
+
+  delete(id: number) {
+    console.log(id);
+  }
+
+  private fetch() {
+    this.tasks$ = this.taskService.getList();
+  }
 }

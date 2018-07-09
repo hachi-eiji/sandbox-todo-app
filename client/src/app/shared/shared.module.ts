3b64b12d875aa4a668bb334223d5abad70cea6b2
Eiji Hachiya <eiji.hachiya@classi.jp>
2018-07-09 21:29:24 +0900

    generate header component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ButtonComponent} from './button/button.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, HeaderComponent],
   exports: [ButtonComponent]
 })
 export class SharedModule { }

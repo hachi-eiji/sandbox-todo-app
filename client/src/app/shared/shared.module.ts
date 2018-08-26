332014f0486e2f987a079caa5f4bfd5a8d411850
Eiji Hachiya <hachiyae@gmail.com>
2018-08-26 12:41:55 +0900

    share module -> core module
    全体で1回しか呼ばないため

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ButtonComponent} from './button/button.component';
-import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ButtonComponent, HeaderComponent],
+  declarations: [ButtonComponent],
   exports: [ButtonComponent]
 })
 export class SharedModule { }

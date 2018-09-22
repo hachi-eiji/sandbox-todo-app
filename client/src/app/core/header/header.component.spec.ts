1fdbb24f816d419656e16263da36d109ff351f54
Eiji Hachiya <hachiyae@gmail.com>
2018-09-22 23:47:43 +0900

    add HeaderComponent test

--- client/src/app/core/header/header.component.spec.ts
+++ client/src/app/core/header/header.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { User } from '../../shared/user/user';
+import { UserService } from '../../shared/user/user.service';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let userService;
 
   beforeEach(async(() => {
+    userService = jasmine.createSpyObj('UserService', ['get']);
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      providers: [{provide: UserService, useValue: userService}]
     })
     .compileComponents();
   }));
@@ -16,10 +23,18 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should get user name', () => {
+    const user: User = {id: 1, name: 'mike'};
+    const spy = userService.get.and.returnValue(of(user));
+    fixture.detectChanges();
+    expect(spy.calls.count()).toEqual(1);
+    expect(fixture.debugElement.query(By.css('header')).nativeElement.textContent).toEqual('mike');
   });
 });

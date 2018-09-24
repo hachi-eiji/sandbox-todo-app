9070c2c03f18f7c2e728e3e8a7ec73452db5ecfa
Eiji Hachiya <hachiyae@gmail.com>
2018-09-25 01:50:46 +0900

    ログイン成功時のActionのテストを追加
    Intellij IDEAのLiveTemplateがES2015に対応してなかったので
    合わせた

--- client/src/app/shared/user/user.reducer.spec.ts
+++ client/src/app/shared/user/user.reducer.spec.ts
@@ -1,12 +1,21 @@
-import { Login } from './user.action';
+import { Login, LoginSuccess } from './user.action';
 import * as userReducer from './user.reducer';
 
 describe('UserReducer', () => {
   describe('LOGIN', () => {
-    it('should return login state', function () {
+    it('should return login state', () => {
       const state: userReducer.UserState = {user: {id: 1, name: 'test'}, error: null};
       const loginAction = new Login({loginId: 'foo', password: 'test'});
       expect(userReducer.userReducer(state, loginAction)).toEqual({user: null, error: null});
+    });
+  });
+
+  describe('LOGIN_SUCCESS', () => {
+    it('should return login success user state', () => {
+      const state: userReducer.UserState = {user: {id: 1, name: 'test'}, error: null};
+      const user = {id: 100, name: 'login success user'};
+      const loginAction = new LoginSuccess({user});
+      expect(userReducer.userReducer(state, loginAction)).toEqual({user, error: null});
     });
   });
 });

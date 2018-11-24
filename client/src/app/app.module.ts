e4daa2fbe16a516db8701e7f7f19ecc54ddd3f1c
Eiji Hachiya <hachiyae@gmail.com>
2018-11-24 17:28:21 +0900

    [client]tasksのstore,effectをlazy load対応
    ユーザ周りは最初から使うので対象外とにする

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,33 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule, MetaReducer, ActionReducer } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LoginModule } from './login/login.module';
+import { metaReducers } from './ngrx-debug';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthEffect } from './shared/user/auth.effect';
 import { UserEffect } from './shared/user/user.effect';
-import { TaskEffect } from './tasks/shared/task.effect';
 import { userReducer } from './shared/user/user.reducer';
-import { taskReducer } from './tasks/shared/task.reducer';
 
 const appRoutes: Routes = [
   { path: 'tasks', loadChildren: 'app/tasks/tasks.module#TasksModule' },
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
-
-// console.log all actions
-export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
-  return function(state, action) {
-    console.log('state', state);
-    console.log('action', action);
-    return reducer(state, action);
-  };
-}
-
-export const metaReducers: MetaReducer<any>[] = [debug];
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
@@ -38,8 +26,8 @@ export const metaReducers: MetaReducer<any>[] = [debug];
     CoreModule,
     FormsModule,
     LoginModule,
-    StoreModule.forRoot({ user: userReducer, task: taskReducer }, { metaReducers }),
-    EffectsModule.forRoot([UserEffect, AuthEffect, TaskEffect]),
+    StoreModule.forRoot({ user: userReducer }, { metaReducers }),
+    EffectsModule.forRoot([UserEffect, AuthEffect]),
     RouterModule.forRoot(appRoutes)
   ],
   bootstrap: [AppComponent]
--- client/src/app/tasks/tasks.module.ts
+++ client/src/app/tasks/tasks.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers } from '../ngrx-debug';
 import { SharedModule } from '../shared/shared.module';
+import { TaskEffect } from './shared/task.effect';
+import { taskReducer } from './shared/task.reducer';
 
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, TasksRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    TasksRoutingModule,
+    StoreModule.forFeature('task', taskReducer, { metaReducers }),
+    EffectsModule.forFeature([TaskEffect])
+  ],
   declarations: [TasksComponent]
 })
 export class TasksModule {}

0ad4e17e3fcf2543065a3b5e32912c049ad6e845
Eiji Hachiya <hachiyae@gmail.com>
2018-11-24 19:57:38 +0900

    [client]update tasks.component.spec.ts

--- client/src/app/tasks/tasks.component.spec.ts
+++ client/src/app/tasks/tasks.component.spec.ts
@@ -1,54 +1,69 @@
 import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { of } from 'rxjs/internal/observable/of';
+import { Store, StoreModule } from '@ngrx/store';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
-import { TaskService } from './shared/task.service';
+import * as TasksAction from './shared/tasks.actions';
 import { Tasks } from './shared/tasks.model';
+import { tasksReducer } from './shared/tasks.reducer';
 
 import { TasksComponent } from './tasks.component';
 
 describe('TasksComponent', () => {
   let component: TasksComponent;
   let fixture: ComponentFixture<TasksComponent>;
-  let taskService;
+  let store: Store<Tasks>;
 
   beforeEach(async(() => {
-    taskService = jasmine.createSpyObj('TaskService', ['getList']);
     TestBed.configureTestingModule({
       declarations: [TasksComponent],
-      imports: [CoreModule, CommonModule, SharedModule],
-      providers: [{
-        provide: TaskService, useValue: taskService
-      }]
+      imports: [
+        CoreModule,
+        CommonModule,
+        SharedModule,
+        StoreModule.forRoot({}),
+        StoreModule.forFeature('tasks', tasksReducer)
+      ]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture = TestBed.createComponent(TasksComponent);
     component = fixture.componentInstance;
   });
 
   it('should show empty message when no task', () => {
-    const data: Tasks = { data: [], status: 200 };
-    const spy = taskService.getList.and.returnValue(of(data));
+    const tasks: Tasks = { data: [], status: 200 };
+    const action = new TasksAction.FetchSuccessAction({ tasks });
+    store.dispatch(action);
+
     fixture.detectChanges(); // onInit()
-    expect(spy.calls.count()).toEqual(1);
+    component.tasks$.subscribe(data => {
+      expect(data.data.length).toEqual(0);
+    });
     expect(fixture.debugElement.query(By.css('.no-task-message'))).not.toBeNull();
+    expect(fixture.debugElement.query(By.css('.m-done'))).toBeNull();
   });
 
   it('should show data', () => {
-    const data: Tasks = {
+    const tasks: Tasks = {
       data: [
         { id: 1, title: 'test title', description: 'description', due_date: '2018/01/01' },
         { id: 1, title: 'test title', description: 'description', due_date: null }
-      ],
-      status: 200
+      ], status: 200
     };
-    taskService.getList.and.returnValue(of(data));
+    const action = new TasksAction.FetchSuccessAction({ tasks });
+    store.dispatch(action);
+
     fixture.detectChanges(); // onInit()
+    component.tasks$.subscribe(data => {
+      expect(data.data.length).toEqual(2);
+    });
     expect(fixture.debugElement.query(By.css('.no-task-message'))).toBeNull();
+    expect(fixture.debugElement.query(By.css('.m-done'))).not.toBeNull();
   });
 });

830490170f6b0ca4375d55995374af1c30575c77
Eiji Hachiya <hachiyae@gmail.com>
2018-12-01 23:33:08 +0900

    factory_botのupdate
    一時的にrubocop-rspecを入れて対応
    
    rubocop \
      --require rubocop-rspec \
      --only FactoryBot/AttributeDefinedStatically \
      --auto-correct

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    title 'task_title'
-    description 'task_description'
+    title { 'task_title' }
+    description { 'task_description' }
     project
     creator_id { User.first.id }
     updater_id { User.first.id }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.safe_email }
-    password 'test'
-    password_confirmation 'test'
-    created_at Time.now
-    updated_at Time.now
+    password { 'test' }
+    password_confirmation { 'test' }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end

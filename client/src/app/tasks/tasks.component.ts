824d1ac1c9c074172f52e1053839001a013dc402
Eiji Hachiya <hachiyae@gmail.com>
2019-10-14 09:55:17 +0900

    タスク削除のイベントを呼び出す

--- client/src/app/tasks/tasks.component.ts
+++ client/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { TaskMessage } from './message/taskMessage.reducer';
 import { TaskFacade } from './shared/task.facade';
 import { Tasks } from './shared/tasks.model';
 
@@ -10,9 +11,11 @@ import { Tasks } from './shared/tasks.model';
 })
 export class TasksComponent implements OnInit {
   tasks$: Observable<Tasks>;
+  message$: Observable<TaskMessage> | null; // ちゃんとnullを入れるべきか?
 
   constructor(private taskFacade: TaskFacade) {
     this.tasks$ = taskFacade.tasks$;
+    this.message$ = taskFacade.message$;
   }
 
   ngOnInit() {
@@ -20,6 +23,6 @@ export class TasksComponent implements OnInit {
   }
 
   delete(id: number) {
-    console.log(id);
+    this.taskFacade.deleteTask(id);
   }
 }

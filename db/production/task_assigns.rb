1ba98268892925f36f229b28616633cfd79bd777
Eiji Hachiya <hachiyae@gmail.com>
2019-10-20 21:57:12 +0900

    add dummy data

---
+++ db/production/task_assigns.rb
@@ -0,0 +1,1 @@
+Task.first.task_assigns.create(user_id: User.first.id)
---
+++ db/production/tasks.rb
@@ -0,0 +1,12 @@
+Project.all.each do |project|
+  5.times.each do |n|
+    task = project.task.build(
+      title:       "title #{n} in project #{project.id}",
+      description: "description #{n} in project #{project.id}",
+      due_date: Time.now,
+      creator_id:  project.user_id,
+      updater_id:  project.user_id
+    )
+    task.save!
+  end
+end

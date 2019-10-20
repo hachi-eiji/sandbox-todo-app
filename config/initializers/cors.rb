2394b207878e080bc321eeefc224dc387e92a226
Eiji Hachiya <hachiyae@gmail.com>
2019-10-20 22:31:24 +0900

    Rails6 から allowの設定が必要
    ついでにCORSも環境変数化

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', '127.0.0.1:4200'
+    origins Settings.cors_origins
 
     resource '*',
       headers: :any,

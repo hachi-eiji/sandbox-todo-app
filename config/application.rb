e09c8233e36325acfc875b30c4d208b93087d855
Eiji Hachiya <hachiyae@gmail.com>
2019-10-21 22:12:00 +0900

    Cookieを使うのでAPIモードをOFFに

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module SandboxTodoApp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
 
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'

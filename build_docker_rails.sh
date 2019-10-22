98cff106250fc6092547eb07cb52bd658962e224
Eiji Hachiya <hachiyae@gmail.com>
2019-10-22 17:33:22 +0900

    AWS対応。dockerでそれぞれをビルドするように変更した

--- build_docker_rails.sh
+++ build_docker_rails.sh
@@ -1,6 +1,6 @@
-docker-compose -f docker-compose.production.yml build \
+docker build -f Dockerfile_rails \
   --build-arg MYSQL_HOST=${MYSQL_HOST} \
   --build-arg MYSQL_PASSWORD=${MYSQL_PASSWORD} \
   --build-arg MYSQL_USER=${MYSQL_USER} \
   --build-arg REDIS_HOST=${REDIS_HOST} \
-  --build-arg RAILS_MASTER_KEY=${RAILS_MASTER_KEY}
+  --build-arg RAILS_MASTER_KEY=${RAILS_MASTER_KEY} -t hachiyae/sandbox-todo-app-rails .

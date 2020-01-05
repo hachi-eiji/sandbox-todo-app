d0c41cf65656e626d601fc42dd6454e8b054b868
Eiji Hachiya <hachiyae@gmail.com>
2020-01-05 23:32:08 +0900

    [mock-api] implements task.findAll

--- mock-api/src/tasks/tasks.service.spec.ts
+++ mock-api/src/tasks/tasks.service.spec.ts
@@ -12,7 +12,25 @@ describe('TasksService', () => {
     service = module.get<TasksService>(TasksService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAll', () => {
+    it('should be all object', () => {
+      const result = [];
+      const date = new Date(2020, 0, 1);
+      for (let i = 0; i < 9; i++) {
+        result.push({
+          id: i * 100,
+          title: `タイトル${i}`,
+          description: `めっちゃ長い説明なんだけど...そうだね説明${i}`,
+          due_date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+        });
+      }
+      result.push({
+        id: 10,
+        title: 'タイトル10',
+        description: '説明10',
+        due_date: null,
+      });
+      expect(service.findAll()).toEqual(result);
+    });
   });
 });
--- mock-api/src/tasks/tasks.service.ts
+++ mock-api/src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  findAll() {
+    const result = [];
+    const date = new Date(2020, 0, 1);
+    for (let i = 0; i < 9; i++) {
+      result.push({
+        id: i * 100,
+        title: `タイトル${i}`,
+        description: `めっちゃ長い説明なんだけど...そうだね説明${i}`,
+        due_date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+      });
+    }
+    result.push({
+      id: 10,
+      title: 'タイトル10',
+      description: '説明10',
+      due_date: null,
+    });
+    return result;
+  }
+}

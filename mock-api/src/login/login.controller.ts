72ff721e160065ee3cab3c0448102f06e54bd12e
Eiji Hachiya <hachiyae@gmail.com>
2020-01-05 22:45:09 +0900

    [mock-api] login のモックをリプレイス

--- mock-api/src/login/login.controller.ts
+++ mock-api/src/login/login.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { LoginService } from './login.service';
 
 @Controller('login')
-export class LoginController {}
+export class LoginController {
+  constructor(private loginService: LoginService) {}
+
+  @Post()
+  login(@Body('loginId') loginId: string, @Body('password') password: string) {
+    const isSuccess = this.loginService.login(loginId, password);
+    if (isSuccess) {
+      return this.successObject();
+    }
+    return this.failedObject();
+  }
+
+  private successObject() {
+    return {
+      status: 200,
+      id: 'user_exists',
+      message: 'ok',
+      data: {
+        id: 1,
+        name: 'テスト太郎',
+      },
+    };
+  }
+
+  private failedObject() {
+    return {
+      status: 404,
+      id: 'login_failed',
+      message: 'ログインID・パスワードが間違っています',
+    };
+  }
+}

b7c59942675c0cfdff135f33215b8834991ef562
Eiji Hachiya <hachiyae@gmail.com>
2020-01-05 22:08:38 +0900

    [mock-api] npm run nest g service login

--- mock-api/src/login/login.module.ts
+++ mock-api/src/login/login.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoginController } from './login.controller';
+import { LoginService } from './login.service';
 
 @Module({
-  controllers: [LoginController]
+  controllers: [LoginController],
+  providers: [LoginService]
 })
 export class LoginModule {}

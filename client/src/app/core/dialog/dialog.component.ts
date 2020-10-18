c8d0d497d0c01be58172369b4ad84e63537f0dee
Eiji Hachiya <hachiyae@gmail.com>
2020-10-18 13:55:18 +0900

    create dialog component

---
+++ client/src/app/core/dialog/dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  open() {
+    console.log('open');
+  }
+}
